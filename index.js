d9524cd5f4d9e67f7c5263a7690fd00a9601a478
Karoki <wanjikukar19@gmail.com>
2025-03-18 07:59:19 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const ramens = [
+const ramen= [
     { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "shoyu.jpg", rating: 5, comment: "Delicious!" },
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
     { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" },
@@ -10,12 +10,13 @@ const ramens = [
 function displayRamens() {
     const ramenMenu = document.getElementById('ramen-menu');
     ramenMenu.innerHTML = '';
+}
     
-    ramens.forEach(ramen => {
+    ramen.forEach(ramen => {
         const ramenItem = document.createElement('div');
         ramenItem.className = 'ramen-item';
         ramenItem.dataset.id = ramen.id;
-        
+    })
         const img = document.createElement('img');
         img.src = ramen.image;
         img.alt = ramen.name;
@@ -28,7 +29,7 @@ function displayRamens() {
     
 
 function handleClick(id) {
-    const ramen = ramens.find(r => r.id === id);
+    const ramen = ramen.find(r => r.id === id);
 }
     if (!ramen) return;
     
